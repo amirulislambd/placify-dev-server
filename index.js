111551899d132f7e29fc7fe853d67e1109eb1fa5
amirulislambd <amirulislambd313@gmail.com>
2026-06-05 03:41:32 +0600

    add job creation endpoint with MongoDB insert

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = 5000
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.MONGO_DB_URI;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const database = client.db("placify_db");
+    const jobCollection = database.collection("jobs");
+
+    // post a job
+    app.post("/api/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
